ad9cb91acb606ef10d950794bcf207874e3c0fdf
Lucas Santana <lucassantana0503@gmail.com>
2026-01-16 22:13:59 -0300

    Atribuindo alguns componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import './styles/global.css'
 
 
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import {Logo} from './components/Logo'
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 
 export function App() {
@@ -11,19 +14,36 @@ export function App() {
   return (
     <>
     <Container>
-      <Heading>LOGO</Heading>
+      <Logo />
     </Container>
 
     <Container>
-      <Heading>MENU</Heading>
+      <Menu />
     </Container>
 
     <Container>
-      <Heading>FORM</Heading>       
+      <CountDown />       
     </Container>
 
     <Container>
-      <Heading>FOOTER</Heading>
+      <form className='form' action="">
+        <div className="formRow">
+          <DefaultInput id='meuInput' type= 'text'/>
+        </div>  
+
+        <div className="formRow">
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div> 
+
+        <div className="formRow">
+          <p>Ciclos</p>
+          <p>0 0 0 0 </p>
+        </div> 
+
+        <div className="formRow">
+          <button>Enviar</button>
+        </div> 
+      </form>    
     </Container>
     </>
   );
