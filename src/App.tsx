af40cf6e3fe86843c08f6bc1a876a4f0556cbe09
Lucas Santana <lucassantana0503@gmail.com>
2026-01-19 17:08:34 -0300

    Criando o Context

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,15 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Home } from "./pages/Home";
-import { useState } from "react";
-import type { TaskStateModel } from "./models/TaskStateModel";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 export function App() {
-  const [state, setState] = useState(initialState);
-
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />;
+    </TaskContextProvider>
+  );
 }
 
 // Que todos os componentes que usam "numero" saibam das mudanças em seu valor
