3b69d83986df0f8deae2280dad2202371db3e5e9
Lucas Santana <lucassantana0503@gmail.com>
2026-01-19 11:41:27 -0300

    Conceito de Prop Drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './styles/theme.css';
-import './styles/global.css'
+import "./styles/theme.css";
+import "./styles/global.css";
 
-import { Home } from './pages/Home';
+import { Home } from "./pages/Home";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
+  const [state, setState] = useState(initialState);
+
+  return <Home />;
+}
+
 // Que todos os componentes que usam "numero" saibam das mudanças em seu valor
 // Sempre que usarmos useState, não vou usar atribuição diretamente
-
-  return (
-    <Home />
-  );
-}
