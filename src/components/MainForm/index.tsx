db1545b302768e9834f4c16322b0b8de65eab4b3
Lucas Santana <lucassantana0503@gmail.com>
2026-01-20 19:30:33 -0300

    Aprendendo mais alguns hooks

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,24 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useRef} from "react";
 
 export function MainForm() {
+const taskNameInput = useRef<HTMLInputElement>(null)
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <form className="form" action="">
+    <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
         <DefaultInput
           id="meuInput"
           type="text"
           labelText="task"
           placeholder="Digite algo"
+          ref={taskNameInput}
         />
       </div>
 
